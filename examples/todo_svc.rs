99c944cb885c37d32565575be9c4b853c9c3c14a
Adam Reichold <adam.reichold@t-online.de>
2018-02-02 13:04:52 +0100

    Use a bit more realistic error handling in the todo_svc example.

--- examples/todo_svc.rs
+++ examples/todo_svc.rs
@@ -10,8 +10,6 @@ extern crate typenum;
 use std::cell::{RefCell, RefMut};
 use std::rc::Rc;
 
-use futures::prelude::*;
-
 #[derive(Serialize, Deserialize)]
 struct Todo {
     text: String,
@@ -25,6 +23,24 @@ struct PersistentTodo {
     done: bool,
 }
 
+#[derive(Debug)]
+enum Error {
+    SerdeJson(serde_json::Error),
+    SerdeOdbc(serde_odbc::Error),
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(err: serde_json::Error) -> Self {
+        Error::SerdeJson(err)
+    }
+}
+
+impl From<serde_odbc::Error> for Error {
+    fn from(err: serde_odbc::Error) -> Self {
+        Error::SerdeOdbc(err)
+    }
+}
+
 struct Service {
     conn: serde_odbc::Connection,
     select_all: serde_odbc::Statement<serde_odbc::NoParams, serde_odbc::RowSet<PersistentTodo>>,
@@ -35,50 +51,47 @@ struct Service {
 struct ServiceHandle(Rc<RefCell<Service>>);
 
 impl Service {
-    fn new(conn_str: &str) -> Self {
-        let env = serde_odbc::Environment::new().unwrap();
-        let conn = serde_odbc::Connection::new(&env, conn_str).unwrap();
+    fn new(conn_str: &str) -> Result<Self, serde_odbc::Error> {
+        let env = serde_odbc::Environment::new()?;
+        let conn = serde_odbc::Connection::new(&env, conn_str)?;
 
-        {
-            let mut stmt: serde_odbc::Statement<
-                serde_odbc::NoParams,
-                serde_odbc::NoCols,
-            > = serde_odbc::Statement::new(
+        let mut create: serde_odbc::Statement<serde_odbc::NoParams, serde_odbc::NoCols> =
+            serde_odbc::Statement::new(
                 &conn,
                 r#"
-                    CREATE TABLE todos (
+                    CREATE TABLE IF NOT EXISTS todos (
                         id INTEGER PRIMARY KEY,
                         text VARCHAR(4096) NOT NULL,
                         done TINYINT NOT NULL
                     )
                 "#,
-            ).unwrap();
-            stmt.exec().unwrap();
-        }
+            )?;
+        create.exec()?;
 
-        let mut select_all =
-            serde_odbc::Statement::new(&conn, "SELECT id, text, done FROM todos").unwrap();
+        let mut select_all = serde_odbc::Statement::new(&conn, "SELECT id, text, done FROM todos")?;
         serde_odbc::FetchSize::set_fetch_size(&mut select_all, 32);
 
         let insert = serde_odbc::Statement::new(
             &conn,
             "INSERT INTO todos (id, text, done) VALUES (?, ?, ?)",
-        ).unwrap();
+        )?;
 
-        Service {
+        Ok(Service {
             conn,
             select_all,
             insert,
-        }
+        })
     }
 
-    fn do_select_all(&mut self, todos: &mut Vec<Todo>) {
+    fn do_select_all(&mut self) -> Result<Vec<Todo>, serde_odbc::Error> {
         let trans = self.conn.begin();
         let stmt = &mut self.select_all;
 
-        stmt.exec().unwrap();
+        stmt.exec()?;
 
-        while stmt.fetch().unwrap() {
+        let mut todos = Vec::new();
+
+        while stmt.fetch()? {
             todos.reserve(stmt.cols().len());
             todos.extend(stmt.cols().iter().map(|todo| Todo {
                 text: String::from_utf8(todo.text.as_slice().unwrap().into()).unwrap(),
@@ -86,39 +99,45 @@ impl Service {
             }));
         }
 
-        trans.commit().unwrap();
+        trans.commit()?;
+
+        Ok(todos)
     }
 
-    fn do_insert(&mut self, todo: &Todo) {
+    fn do_insert(&mut self, todo: &Todo) -> Result<(), serde_odbc::Error> {
         let trans = self.conn.begin();
         let stmt = &mut self.insert;
 
         stmt.params().text.assign(todo.text.as_bytes());
         stmt.params().done = todo.done;
 
-        stmt.exec().unwrap();
+        stmt.exec()?;
 
-        trans.commit().unwrap();
+        trans.commit()?;
+
+        Ok(())
     }
 
-    fn get_todos(&mut self) -> Vec<u8> {
-        let mut todos = Vec::new();
+    fn get_todos(&mut self) -> Result<Vec<u8>, Error> {
+        let todos = self.do_select_all()?;
+        let todos = serde_json::to_vec(&todos)?;
 
-        self.do_select_all(&mut todos);
-
-        serde_json::to_vec(&todos).unwrap()
+        Ok(todos)
     }
 
-    fn post_todo(&mut self, todo: &[u8]) {
-        let todo = serde_json::from_slice(todo).unwrap();
+    fn post_todo(&mut self, todo: &[u8]) -> Result<(), Error> {
+        let todo = serde_json::from_slice(todo)?;
+        self.do_insert(&todo)?;
 
-        self.do_insert(&todo);
+        Ok(())
     }
 }
 
 impl ServiceHandle {
-    fn new(conn_str: &str) -> Self {
-        ServiceHandle(Rc::new(RefCell::new(Service::new(conn_str))))
+    fn new(conn_str: &str) -> Result<Self, serde_odbc::Error> {
+        Ok(ServiceHandle(Rc::new(RefCell::new(Service::new(
+            conn_str,
+        )?))))
     }
 
     fn get_mut(&self) -> RefMut<Service> {
@@ -126,42 +145,57 @@ impl ServiceHandle {
     }
 }
 
+fn response_with_status(status: hyper::StatusCode) -> hyper::server::Response {
+    hyper::server::Response::new().with_status(status)
+}
+
+fn response_with_body(body: Vec<u8>) -> hyper::server::Response {
+    hyper::server::Response::new()
+        .with_header(hyper::header::ContentType::json())
+        .with_header(hyper::header::ContentLength(body.len() as u64))
+        .with_body(body)
+}
+
+fn boxed_response(
+    resp: hyper::server::Response,
+) -> Box<futures::Future<Item = hyper::server::Response, Error = hyper::Error>> {
+    Box::new(futures::future::ok(resp))
+}
+
 impl hyper::server::Service for ServiceHandle {
     type Request = hyper::server::Request;
     type Response = hyper::server::Response;
     type Error = hyper::Error;
-    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
+    type Future = Box<futures::Future<Item = Self::Response, Error = Self::Error>>;
 
     fn call(&self, req: Self::Request) -> Self::Future {
         match (req.method(), req.path()) {
             (&hyper::Method::Get, "/todos") => {
-                let todos = self.get_mut().get_todos();
-
-                Box::new(futures::future::ok(
-                    hyper::server::Response::new()
-                        .with_header(hyper::header::ContentType::json())
-                        .with_header(hyper::header::ContentLength(todos.len() as u64))
-                        .with_body(todos),
-                ))
+                let resp = match self.get_mut().get_todos() {
+                    Ok(todos) => response_with_body(todos),
+                    Err(_) => response_with_status(hyper::StatusCode::InternalServerError),
+                };
+                boxed_response(resp)
             }
             (&hyper::Method::Post, "/todos") => {
+                use futures::{Future, Stream};
                 let svc = self.clone();
 
                 Box::new(req.body().concat2().map(move |body| {
-                    svc.get_mut().post_todo(body.as_ref());
-
-                    hyper::server::Response::new()
+                    let status = match svc.get_mut().post_todo(body.as_ref()) {
+                        Ok(()) => hyper::StatusCode::Ok,
+                        Err(_) => hyper::StatusCode::InternalServerError,
+                    };
+                    response_with_status(status)
                 }))
             }
-            _ => Box::new(futures::future::ok(
-                hyper::server::Response::new().with_status(hyper::StatusCode::NotFound),
-            )),
+            _ => boxed_response(response_with_status(hyper::StatusCode::NotFound)),
         }
     }
 }
 
 fn main() {
-    let svc = ServiceHandle::new("Driver=Sqlite3;Database=:memory:;");
+    let svc = ServiceHandle::new("Driver=Sqlite3;Database=todos.db;").unwrap();
 
     let server = hyper::server::Http::new()
         .bind(&([127, 0, 0, 1], 8080).into(), move || Ok(svc.clone()))
--- src/error.rs
+++ src/error.rs
@@ -8,8 +8,8 @@ use serde::ser;
 
 #[derive(Debug)]
 pub enum Error {
-    OdbcError(SQLRETURN),
-    SerdeError(String),
+    Odbc(SQLRETURN),
+    Serde(String),
 }
 
 pub type Result<T> = result::Result<T, Error>;
@@ -17,8 +17,8 @@ pub type Result<T> = result::Result<T, Error>;
 impl error::Error for Error {
     fn description(&self) -> &str {
         match self {
-            &Error::OdbcError(_) => "ODBC error",
-            &Error::SerdeError(_) => "Serde error",
+            &Error::Odbc(_) => "ODBC error",
+            &Error::Serde(_) => "Serde error",
         }
     }
 }
@@ -26,15 +26,15 @@ impl error::Error for Error {
 impl fmt::Display for Error {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> result::Result<(), fmt::Error> {
         match self {
-            &Error::OdbcError(rc) => write!(fmt, "ODBC error: {:?}", rc),
-            &Error::SerdeError(ref msg) => write!(fmt, "Serde error: {}", msg),
+            &Error::Odbc(rc) => write!(fmt, "ODBC error: {:?}", rc),
+            &Error::Serde(ref msg) => write!(fmt, "Serde error: {}", msg),
         }
     }
 }
 
 impl ser::Error for Error {
     fn custom<T: fmt::Display>(msg: T) -> Self {
-        Error::SerdeError(msg.to_string())
+        Error::Serde(msg.to_string())
     }
 }
 
@@ -46,7 +46,7 @@ impl OdbcResult for SQLRETURN {
     fn check(self) -> Result<()> {
         match self {
             SQL_SUCCESS | SQL_SUCCESS_WITH_INFO | SQL_NO_DATA => Ok(()),
-            rc => Err(Error::OdbcError(rc)),
+            rc => Err(Error::Odbc(rc)),
         }
     }
 }
