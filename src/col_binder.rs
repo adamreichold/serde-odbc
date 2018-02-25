b1016587ac4b001045f18bda5449959bdea04546
Adam Reichold <adam.reichold@t-online.de>
2018-02-25 21:27:53 +0100

    Add LGPL license and preamble and extend Cargo metadata.

--- src/col_binder.rs
+++ src/col_binder.rs
@@ -1,3 +1,19 @@
+/*
+This file is part of serde-odbc.
+
+serde-odbc is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+serde-odbc is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with serde-odbc.  If not, see <http://www.gnu.org/licenses/>.
+*/
 use std::mem::size_of;
 
 use odbc_sys::{SQLBindCol, SQLHSTMT, SQLLEN, SQLPOINTER, SQLUSMALLINT, SQL_C_CHAR};
